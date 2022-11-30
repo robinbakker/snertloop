32383dc9efc94deac72583062ced9e68722e3f44
Robin Bakker <11250019+robinbakker@users.noreply.github.com>
2022-11-30 22:26:51 +0100

    first commit

---
+++ public/index.js
@@ -0,0 +1,27 @@
+import { render } from 'preact';
+import Routes from './routes';
+import Map from './map';
+
+function App() {
+	return (
+		<main>
+			<fieldset>
+				<input id="t16" type="checkbox" checked />
+				<label for="t16">16km</label>
+				<input id="t11" type="checkbox" checked />
+				<label for="t11">11km</label>
+				<input id="t5" type="checkbox" checked />
+				<label for="t5">5km</label>
+				<input id="tc" type="checkbox" checked />
+				<label for="tc">Verkeersregelaar</label>
+			</fieldset>
+			<div class="map-container">
+				{/* <Map /> */}
+				<img src="/assets/map.svg" alt="map" />
+				<Routes />
+			</div>
+		</main>
+	);
+}
+
+render(<App />, document.body);
